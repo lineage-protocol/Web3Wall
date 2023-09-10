bf3edd9dbb39369781c9447cc5adaae2dfe14741
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-09-10 20:27:44 +0800

    update `/dashboard`

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
+import GenericButton from 'components/Buttons/GenericButton'
 import EventCard from 'components/EventCard'
-import { useGetSheets } from 'repositories/subgraph.repository'
+import MintModal from 'components/Modal/MintModal'
+import { useState } from 'react'
+import { useGetTransactions } from 'repositories/rpc.repository'
+import { useGetEvents } from 'repositories/subgraph.repository'
 
 const items = [
   {
@@ -25,27 +29,80 @@ const items = [
 ]
 
 const PageDashboard = () => {
-  const { data: sheets } = useGetSheets({})
-  console.log(sheets)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const { data: events } = useGetEvents({})
+  const { data: txs } = useGetTransactions({
+    query: [
+      {
+        column: 'method',
+        op: '=',
+        query: 'metadata',
+      },
+      {
+        column: 'status',
+        op: '=',
+        query: '1',
+      },
+      {
+        column: 'meta_contract_id',
+        op: '=',
+        query: `${import.meta.env.VITE_WEB3WALL_META_CONTRACT_ID}`,
+      },
+    ],
+    ordering: [
+      {
+        column: 'timestamp',
+        sort: 'desc',
+      },
+    ],
+    from: 0,
+    to: 0,
+  })
 
   return (
     <div className="bg-yellow-100 h-screen">
+      <GenericButton className="ml-4 mt-4" name="Mint" onClick={() => openModal()} />
       <div className="grid gap-3 p-3">
-        {items.map((item, index) => {
-          return (
-            <EventCard
-              key={index}
-              tokenAddress={item.token.token_address}
-              chainId={item.token.chain_id}
-              tokenId={item.token.token_id}
-              title={item.title}
-              onHandleShareClicked={() => {}}
-              totalUser={item.users}
-              totalPost={item.posts}
-            />
-          )
-        })}
+        {txs && txs.length > 0
+          ? txs.map((item, index) => {
+              return (
+                <EventCard
+                  key={index}
+                  tokenAddress={item.token_address}
+                  chainId={item.chain_id}
+                  tokenId={item.token_id}
+                  title={events?.[`${item.data_key}`].data?.name}
+                  onHandleShareClicked={() => {}}
+                  totalUser={0}
+                  totalPost={0}
+                />
+              )
+            })
+          : items.map((item, index) => {
+              return (
+                <EventCard
+                  key={index}
+                  tokenAddress={item.token.token_address}
+                  chainId={item.token.chain_id}
+                  tokenId={item.token.token_id}
+                  title={item.title}
+                  onHandleShareClicked={() => {}}
+                  totalUser={item.users}
+                  totalPost={item.posts}
+                />
+              )
+            })}
       </div>
+      <MintModal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   )
 }
